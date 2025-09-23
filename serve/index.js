178780308c234210a8d2d2597f108d6a0ba6b087
TwoSMallMie <Mie2540769619@outlook.com>
2025-09-24 00:12:23 +0800

    修复了服务器的一些bug

--- serve/index.js
+++ serve/index.js
@@ -1,35 +1,15 @@
 
 const express = require('express');
 const cors = require('cors');
-<<<<<<< HEAD
 const mysql = require('mysql');
-=======
 const { db } = require('./db');
 const userController = require('./controller/userController');
 const qiniuController = require('./controller/qiniuController');
->>>>>>> master
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-<<<<<<< HEAD
-// 创建 MySQL 连接
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456', // 请根据实际情况修改
-  database: 'testdb'   // 请确保已创建 testdb 数据库
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('数据库连接失败:', err);
-  } else {
-    console.log('数据库连接成功');
-  }
-});
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from serve backend!' });
@@ -47,7 +27,6 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-=======
 //test api
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello Node serve!' });
@@ -72,7 +51,6 @@ app.post('/api/qiniu/chat-stream', qiniuController.chatStreamModel);
 
 
 // node serve 启动
->>>>>>> master
 app.listen(port, () => {
   console.log(`Serve backend running at http://localhost:${port}`);
 });
